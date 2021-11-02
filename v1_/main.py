669d5dcb0a6b7afa6f00f3c104611326c71d8cf6
BlueRexPY <66295121+BlueRexPY@users.noreply.github.com>
2021-11-02 22:09:28 +0200

    добавлен генератор db, секретная функция, data,db map для постранения db

--- v1_/main.py
+++ v1_/main.py
@@ -3,24 +3,39 @@ from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 
 from config import TOKEN
-
+from data import VOICES,IMG
+import sqlite3
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
 
+#commands
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
-    await message.reply("Добро пожаловать в магазин-темплейт)
-
+    await message.reply("Добро пожаловать в магазин-темплейт. Для просмотра каталога напишите /shop ")
 
 @dp.message_handler(commands=['help'])
 async def process_help_command(message: types.Message):
     await message.reply("Для просмотра каталога напишите /shop")
 
+@dp.message_handler(commands=['shop'])
+async def process_help_command(message: types.Message):
+    await message.reply("Нажмите на одну из кнопок")
 
+#SECRET
+@dp.message_handler(commands=['tof'])
+async def process_voice_command(message: types.Message):
+    for i in range(len(VOICES[0])):
+        await bot.send_voice(message.from_user.id, VOICES[1][i], caption=VOICES[0][i])
+
+
+
+
+#text
 @dp.message_handler()
-async def echo_message(msg: types.Message):
+async def echo_message(msg: "Hi"):
+    print(f"{msg.from_user.username}: {msg.text}")
     await bot.send_message(msg.from_user.id, msg.text)
 
 
